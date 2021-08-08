a71464582f9ac55a3e166ca2d75e6332dd797b52
otinu <fitshter@gmail.com>
2021-08-08 11:49:23 +0900

    [finish]Chapter-11

---
+++ sample02.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+print(2**0 + 2**1 + 2**2 + 2**3 + 2**4 + 2**5 + 2**6 + 2**7);
+?>
+</pre>
+</main>
+</body>
+</html>
