d80f3b777d0f06d41faf99d066aa748fbee014c3
otinu <fitshter@gmail.com>
2021-08-11 23:16:27 +0900

    [finish]Chapter23

---
+++ sample11.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+
+切り上げ(ceil) ⇒ <?php print(ceil(2.564)); ?>
+
+切り捨て(floor)<?php print(floor(5.9124)); ?>
+
+四捨五入(round) ⇒ <?php print(round(1.4567, 3)); ?>
+
+</pre>
+</main>
+</body>
+</html>
