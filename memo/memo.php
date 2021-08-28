7bc394be1914dc11f28d283b895b3a9b349e08a4
otinu <fitshter@gmail.com>
2021-08-28 17:58:15 +0900

    [finish]Chapter63

--- memo/memo.php
+++ memo/memo.php
@@ -28,7 +28,14 @@ try {
   echo 'Connection failed: ' . $e->getMessage();
 } 
 
-$memos = $db->query('SELECT * FROM memos WHERE id=1');
+$id = $_REQUEST['id'];
+if (!is_numeric($id) || $id <= 0 ) {
+  print('1以上の数字で指定してください');
+  exit();
+}
+
+$memos = $db->prepare('SELECT * FROM memos WHERE id=?');
+$memos->execute(array($id));
 $memo = $memos->fetch();
 ?>
 
