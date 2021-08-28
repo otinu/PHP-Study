da9f0846c58a07788155de8f4d430b15f854e22d
otinu <fitshter@gmail.com>
2021-08-28 12:20:47 +0900

    [fix]Chapter58/59

--- memo/index.php
+++ memo/index.php
@@ -29,12 +29,20 @@ try {
   echo 'Connection failed: ' . $e->getMessage();
 } 
 
+/* Chapter58
+$count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰、ピンク、甘い"');
+echo $count . '件のデータを挿入しました';
+?>
+*/
+
+/* Chapter59
 $records = $db->query('SELECT * FROM my_items');
 while ($record = $records->fetch()) {
   print($record['item_name'] . "\n");
 }
+*/
 
-?>
+
 </pre>
 </main>
 </body>
